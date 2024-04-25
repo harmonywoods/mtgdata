885a4dd12ed170cc3f5c6cc44247b35bb6a51aca
harmonywoods <140229467+harmonywoods@users.noreply.github.com>
2024-04-24 22:12:14 -0700

    added more information
    might add date/time later

--- input.py
+++ input.py
@@ -5,9 +5,12 @@ will put more words here later
 import csv
 filename = input("what file should this info be put into?")
 opp = input("Opponent Name:")
+format = input("Format:")
+selfdeck = input("Your Deck (Use Guild/Shard names):")
 oppdeck = input("Opponent Deck (Use Guild/Shard names):")
+event = input("Event:")
 result = input("Game Results e.g. WLW:")
 notes = input("Notes:")
-info = ",".join([opp,oppdeck,result,notes])
+info = ",".join([opp,format,selfdeck,oppdeck,event,result,notes])
 with open(filename, mode = 'a') as datafile:
     print(info, file=datafile)
